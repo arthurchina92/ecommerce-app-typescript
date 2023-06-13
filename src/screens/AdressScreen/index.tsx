6b81cb887a81105e76cb28a5a0ddee86a64cbafb
arthurchina92 <arthurchinaa@gmail.com>
2023-06-12 19:04:18 -0600

    AddressScreen and validation of fields

--- src/screens/AdressScreen/index.tsx
+++ src/screens/AdressScreen/index.tsx
@@ -1,23 +1,116 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TextInput, Alert} from 'react-native';
+import React, {useState} from 'react';
 import styles from './styles';
 import {Picker} from '@react-native-picker/picker';
 import countrylist from 'country-list';
+import Button from '../../components/Button';
 
 const countries = countrylist.getData();
 
 const AdressScreen = () => {
-  console.log(countries);
+  const [country, setCountry] = useState(countries[0].code);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [city, setCity] = useState('');
+  const [line1, setLine1] = useState('');
+  const [line2, setLine2] = useState('');
+  const [line1Error, setLine1Error] = useState('Invalid address');
+  const [line2Error, setLine2Error] = useState('Invalid address');
+  const onCheckout = () => {
+    if (!name) {
+      Alert.alert('Please fill in the fullname field');
+      return;
+    } else if (!phone) {
+      Alert.alert('Please fill in the phone number field');
+      return;
+    } else if (!line1 || !line2) {
+      Alert.alert('Please fill in the Adress field');
+      return;
+    }
+    console.warn('Checked out baby!');
+  };
+  const validateLine1 = () => {
+    if (line1.length < 4) {
+      setLine1Error('Address is too short');
+    } else if (line1.length > 30) {
+      setLine1Error('Address is too long');
+    }
+  };
+  const validateLine2 = () => {
+    if (line2.length < 4) {
+      setLine2Error('Address is too short');
+    } else if (line2.length > 30) {
+      setLine2Error('Address is too long');
+    }
+  };
 
   return (
-    <View>
-      <View style={styles.row}>
-        <Picker>
+    <View style={styles.root}>
+      <View style={styles.container}>
+        <Picker selectedValue={country} onValueChange={setCountry}>
           {countries.map(country => (
-            <Picker.Item value={country.code} label={country.name} />
+            <Picker.Item
+              value={country.code}
+              label={country.name}
+              key={country.code}
+            />
           ))}
         </Picker>
       </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>Full name (First and last name)</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Full name"
+          value={name}
+          onChangeText={setName}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>Phone number</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Phone number"
+          value={phone}
+          onChangeText={setPhone}
+          keyboardType="phone-pad"
+        />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>Address</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Line 1"
+          value={line1}
+          onEndEditing={validateLine1}
+          onChangeText={text => {
+            setLine1(text);
+            setLine1Error('');
+          }}
+        />
+        {!!line1Error && <Text style={styles.errorLabel}>{line1Error}</Text>}
+        <TextInput
+          style={styles.input}
+          placeholder="Line 2"
+          value={line2}
+          onEndEditing={validateLine2}
+          onChangeText={text => {
+            setLine2(text);
+            setLine2Error('');
+          }}
+        />
+        {!!line2Error && <Text style={styles.errorLabel}>{line2Error}</Text>}
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>City</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="City"
+          value={city}
+          onChangeText={setCity}
+        />
+      </View>
+      <Button text="Checkout" onPress={onCheckout} />
     </View>
   );
 };
--- src/screens/AdressScreen/styles.ts
+++ src/screens/AdressScreen/styles.ts
@@ -1,7 +1,30 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  root: {},
   row: {
     flexDirection: 'row',
   },
+  container: {
+    marginLeft: 20,
+    marginVertical: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  errorLabel: {
+    color: 'red',
+    marginLeft: 10,
+  },
 });
+
+export default styles;
